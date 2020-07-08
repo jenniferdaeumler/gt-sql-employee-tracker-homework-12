0ba8131bd99993c2cb6c35614b892b27a7977c43
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-07 20:07:28 -0500

    Seed SQL file started

---
+++ seed.sql
@@ -0,0 +1,45 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department(
+    id INT AUTO_INCREMENT,
+    name VARCHAR (30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+    id INT AUTO_INCREMENT,
+    title VARCHAR (30),
+    salary DECIMAL,
+    department_id INT,
+     PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+    first_name VARCHAR (30),
+    last_name VARCHAR (30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
+
+
+
+
+INSERT INTO songs (title, artist, genre)
+VALUES ("Sweet Home Alabama", "Lynyrd Skynrd", "Southern Rock");
+
+
+INSERT INTO songs (title, artist, genre)
+VALUES ("Oops I Did It Again", "Britney Spears", "Pop");
+
+
+INSERT INTO songs (title, artist, genre)
+VALUES ("Thunder Rolls", "Garth Brooks", "Country");
+
+SELECT * FROM songs
+--Only songs from beatles?
+WHERE arist = "Beatles";
