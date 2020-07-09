6b7688a5fd0173468aaa6874e9d18758f9a8a817
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-09 15:56:29 -0500

    fixed daryls last name

--- seed.sql
+++ seed.sql
@@ -37,7 +37,7 @@ INSERT INTO role (title, salary, department_id)
 VALUES ("Sales Lead", 60000, 1), ("Sales Person", 40000, 1), ("Lead Engineer", 100000, 2), ("Software Engineer", 70000, 2), ("Account Manager", 50000, 1), ("Accountant", 60000, 3), ("Legal Team Lead", 150000, 4), ("Lawyer", 90000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Daryl", "Reedus",1,null), ("Rick", "Grimes",2,1), ("Negan", "Wilson",3,null), ("Carol", "Peletier",4,3), ("Maggie", "Green",5,1), ("Michonne", "Hawthorne",6,null), ("Eugene", "Porter",7,null), ("Rosita", "Espinosa",8,7);
+VALUES ("Daryl", "Dixon",1,null), ("Rick", "Grimes",2,1), ("Negan", "Wilson",3,null), ("Carol", "Peletier",4,3), ("Maggie", "Green",5,1), ("Michonne", "Hawthorne",6,null), ("Eugene", "Porter",7,null), ("Rosita", "Espinosa",8,7);
 
 SELECT * FROM department;
 SELECT * FROM role;
