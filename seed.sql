4c950a905735191a452ede7e227a9f8e2756848d
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-09 16:07:26 -0500

    'What would you like to do?' question created and logged

--- seed.sql
+++ seed.sql
@@ -55,3 +55,12 @@ ON role.department_id = department.id;
 --id, first_name, last_name, title, department, salary, manager
 --Departments: Sales 1, Engineering 2, Finance 3, Legal 4
 --Roles: Sales Lead 1, Salesperson 2, Lead Engineer 3, Software Engineer 4, Account Manager 5, Accountant 6, Legal Team Lead 7, Lawyer 8
+
+-- **Q: What would you like to do? O:View All Employees, View All Employees by Department, View All Employees by Manager,Add Employee, Remove Employee, Update Employee Role, Update Employee Manager
+-- Add Employee Questions--> What is the employee's first name? What is the employee's last name? What is the employee's role? (List of options: Sales Lead, Salesperson, Lead Engineer, Software Engineer, Account Manager, Accountant, Legal Team Lead, Lawyer) Who is the employee's manager? (List of manager options from DB)
+-- Remove Employee Questions: --> Which employee do you wish to remove? (List of employees from DB)
+-- Update Employee Manager Questions: --> Which employee's manager do you wish to update? (List of employees from DB) Which employee do you wish to set as manager for selected employee? (List of employees from DB)
+-- Update Employee Role Questions: --> Which employee's role do you wish to update? (List of employees from DB) Which role do you wish to set for employee? (List of roles from DB)
+-- View All Employees by Department --> Select a Department (List from DB) 
+-- View All Employees by Manager --> Select a Manager (List from DB) 
+-- Departments: Sales, Engineering, Finance, Legal
--- server.js
+++ server.js
@@ -17,20 +17,27 @@ var connection = mysql.createConnection({
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-//   functionName();
-connection.end();
+  userPrompt();
 });
 
-
-//Start inquirer prompt
-//**Q: What would you like to do? O:View All Employees, View All Employees by Department, View All Employees by Manager,Add Employee, Remove Employee, Update Employee Role, Update Employee Manager
-//Add Employee Questions--> What is the employee's first name? What is the employee's last name? What is the employee's role? (List of options: Sales Lead, Salesperson, Lead Engineer, Software Engineer, Account Manager, Accountant, Legal Team Lead, Lawyer) Who is the employee's manager? (List of manager options from DB)
-//Remove Employee Questions: --> Which employee do you wish to remove? (List of employees from DB)
-//Update Employee Manager Questions: --> Which employee's manager do you wish to update? (List of employees from DB) Which employee do you wish to set as manager for selected employee? (List of employees from DB)
-//Update Employee Role Questions: --> Which employee's role do you wish to update? (List of employees from DB) Which role do you wish to set for employee? (List of roles from DB)
-//View All Employees by Department --> Select a Department (List from DB) 
-//View All Employees by Manager --> Select a Manager (List from DB) 
-//Departments: Sales, Engineering, Finance, Legal
+//Start inquirer prompts
+//What would you like to do list?
+function userPrompt() {
+  // prompt for info about the item being put up for auction
+  inquirer
+    .prompt([
+      {
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["View All Employees", "View All Employees by Department", "View All Employees by Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager"]
+      }
+    ])
+    .then(function (answer) {
+      console.log(answer)
+      connection.end();
+    })
+};
