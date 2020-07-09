9a401e2cac6fb1df5f42ef45b39d65980c7343a9
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-09 16:29:27 -0500

    Conditionals created for all action choices. Pseudocode functions to make

--- server.js
+++ server.js
@@ -32,15 +32,44 @@ function userPrompt() {
         name: "action",
         type: "list",
         message: "What would you like to do?",
-        choices: ["View All Employees", "View All Employees by Department", "View All Employees by Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager"]
+        choices: ["View All Employees", "View All Employees by Department", "View All Employees by Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "Exit"]
       }
     ])
     .then(function (answer) {
-      console.log(answer.action)
+      // console.log(answer.action)
       //Conditional for selected action
       if (answer.action === "View All Employees") {
         //If 'View All Employees' is selected, call viewAll() function
         viewAll();
+      }
+      //If 'View All Employees by Dept' is selected, call viewDept() function
+      else if(answer.action === "View All Employees by Department"){
+        console.log("View All Employees by Department Selected")
+      }
+      //If 'View All Employees by Manager' is selected, call viewManager() function
+      else if(answer.action === "View All Employees by Manager"){
+        console.log("View All Employees by Manager Selected")
+      }
+      //If 'Add Employee' is selected, call addEmployee() function
+      else if(answer.action === "Add Employee"){
+        console.log("Add Employee Selected")
+      }
+      //If 'Remove Employee' is selected, call removeEmployee() function
+      else if(answer.action === "Remove Employee"){
+        console.log("Remove Employee Selected")
+      }
+      //If 'Update Employee Role' is selected, call updateRole() function
+      else if(answer.action === "Update Employee Role"){
+        console.log("Update Employee Role Selected")
+      }
+      //If 'Update Employee Manager' is selected, call updateManager() function
+      else if(answer.action === "Update Employee Manager"){
+        console.log("Update Employee Manager Selected")
+      }
+      //If 'Exit' is selected, return userPrompt()
+      else{
+        console.log("Exit Selected")
+        return userPrompt();
       }
       connection.end();
     })
