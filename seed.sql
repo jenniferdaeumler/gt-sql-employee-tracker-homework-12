9a8643f7b15faa375aebb7d2c0af83c8c216795a
Jennifer Daeumler <jenniferdaeumler@gmail.com>
2020-07-09 08:26:17 -0500

    SQL seed working

--- seed.sql
+++ seed.sql
@@ -34,17 +34,11 @@ CREATE TABLE employee(
 INSERT INTO department (name)
 VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
 
-INSERT INTO role (title, salary)
-VALUES ("Sales Lead", 60000), ("Sales Person", 40000), ("Lead Engineer", 100000), ("Software Engineer", 70000), ("Account Manager", 50000), ("Accountant", 60000), ("Legal Team Lead", 150000), ("Lawyer", 90000);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 60000, 1), ("Sales Person", 40000, 1), ("Lead Engineer", 100000, 2), ("Software Engineer", 70000, 2), ("Account Manager", 50000, 1), ("Accountant", 60000, 3), ("Legal Team Lead", 150000, 4), ("Lawyer", 90000, 4);
 
-INSERT INTO employee (first_name, last_name)
-VALUES ("Daryl", "Reedus"), ("Rick", "Grimes"), ("Negan", "Wilson"), ("Carol", "Peletier"), ("Maggie", "Green"), ("Michonne", "Hawthorne"), ("Eugene", "Porter"), ("Rosita", "Espinosa");
-
---If needed:
--- INSERT INTO employee (first_name, last_name)
--- VALUES ("Daryl", "Reedus",1), ("Rick", "Grimes",2,1), ("Negan", "Wilson",3), ("Carol", "Peletier",4,3), ("Maggie", "Green",5,1), ("Michonne", "Hawthorne",6), ("Eugene", "Porter",7), ("Rosita", "Espinosa",8,7);
-
-
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Daryl", "Reedus",1,null), ("Rick", "Grimes",2,1), ("Negan", "Wilson",3,null), ("Carol", "Peletier",4,3), ("Maggie", "Green",5,1), ("Michonne", "Hawthorne",6,null), ("Eugene", "Porter",7,null), ("Rosita", "Espinosa",8,7);
 
 SELECT * FROM department;
 SELECT * FROM role;
